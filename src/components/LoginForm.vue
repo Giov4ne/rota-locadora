f051a99bd690807b2e3a7d816241cb64de5a9fa7
Giovane William Budal <giovanewbudal@gmail.com>
2025-02-21 20:38:24 -0300

    adding LoginForm component and background image

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="login-signin-container">
+        <!-- <form action="">
+            <input type="text"><br>
+            <input type="text"><br>
+            <input type="text"><br>
+            <button>Enviar</button>
+        </form> -->
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'LoginForm'
+    }
+</script>
+
+<style>
+    #login-signin-container{
+        background-image: url('../assets/cars-background.png');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+        background-position: center;
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #ececec;
+    }
+
+    /* form{
+        background-color: white;
+        width: 300px;
+    } */
+</style>
